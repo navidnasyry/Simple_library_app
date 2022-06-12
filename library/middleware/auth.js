76924a36faf9509e4d24189cd00ac0990d087b79
NavidNasyry <navidnasyry@gmail.com>
2022-06-12 16:09:54 +0430

    edit middleware and get token from bearer token

--- library/middleware/auth.js
+++ library/middleware/auth.js
@@ -3,8 +3,18 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-    const token =
-        req.body.token || req.query.token || req.headers["x-access-token"];
+
+    let pre_token = req.headers.authorization;
+    let token = null;
+    if (pre_token){ // if use bearer token
+        if(pre_token.toLowerCase().startsWith('bearer')){
+            token = req.headers.authorization.slice('bearer'.length).trim();
+        }
+    }
+    else{ // if use token field from body or query or x-access-token field from headers
+        token =
+            req.body.token || req.query.token || req.headers["x-access-token"];
+    }  
 
     if (!token) {
         return res.status(403).send("A token is required for authentication -_-");
